31e1f523aa1cfdb56fc3a752a5e72d93e86851d2
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-06-08 16:52:57 -0700

    a program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    num = len(argv)
+    print("{] argument{}:".format(num, 's' if num != 1 else ''))
+
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
