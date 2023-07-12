38437aecc620448e8de3a1e4667e654ba81a1e49
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-07-11 19:49:52 -0700

    a class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module of the class
+"""
+
+
+class Student:
+    """
+    class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiate the class Student
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student
+        """
+        return self.__dict__
