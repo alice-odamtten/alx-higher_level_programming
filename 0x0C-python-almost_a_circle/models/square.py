834e334c63ff4bfdf9e511656f1dd200ad3c55d3
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-07-15 11:44:05 -0700

    Update the class Square by adding the public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,17 +8,17 @@ class Square(Rectangle):
     """ Square class, inherits from Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Constructor method for Square class """
+        """ method for Square class """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """ Getter for size attribute """
+        """ Get the size attribute """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Setter for size attribute """
+        """ Set the size attribute """
         self.width = value
         self.height = value
 
@@ -28,3 +28,41 @@ class Square(Rectangle):
                                                  self.x,
                                                  self.y,
                                                  self.width)
+
+    @property
+    def width(self):
+        """ Get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for width attribute
+
+        Args:
+            value: width value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for height attribute
+
+        Args:
+            value: height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
