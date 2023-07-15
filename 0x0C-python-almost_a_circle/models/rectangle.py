e40fd87c0aa989af8dd82016c68fd213cf5839b6
alice-odamtten <sakyibeaodamtten@gmail.com>
2023-07-15 09:01:51 -0700

    added the display of '#'

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,6 +41,10 @@ class Rectangle(Base):
         Args:
             value: width value
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -56,6 +60,10 @@ class Rectangle(Base):
         Args:
             value: height value
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -71,6 +79,10 @@ class Rectangle(Base):
         Args:
             value: value of x
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -86,4 +98,17 @@ class Rectangle(Base):
         Args:
             value: value of y
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Calculate and return the area of the rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """ Print the rectangle with the character '#' """
+        for _ in range(self.__height):
+            print("#" * self.__width)
