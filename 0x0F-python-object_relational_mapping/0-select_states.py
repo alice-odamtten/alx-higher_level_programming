e1c3cd0f5cc333ce158d1ed73fdbd2db14660e33
alice-odamtten <sakyibeaoamtten@gmail.com>
2023-09-17 18:53:47 -0700

    a script that lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """Defines a method to get states"""
+        connection = MySQLdb.connect(
+                host="localhost", port=3306, user=argv[1], passwd=argv[2],
+                db=argv[3]
+        )
+	sqlfomula = "SELECT * FROM states ORDER BY states.id ASC"
+	cursor.execute(sqlfomula)
+	myresult= cursor.fetchall()
+	for i in myresult:
+		print(i)
+
+	cursor.close()
+	connection.close()
